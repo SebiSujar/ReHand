a18d5036594b9930f27a7ff9cff35e1d08f9279f
Roman Suarez <romandelsolar@gmail.com>
2014-11-05 05:26:46 -0300

    5.01 de la mañana, estoy quemadisimo

--- server/appConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	api_key: 'ruXFonJxX4ZGRhkcoiLLo0hhs',
-	api_secret: 'NYLgyj1JG4EG7JTea5Y9WXlXYOg6MupEsthLOTGWB8AlPUkI6Y',
-  access_token: '630725899-di5DY4FQa3Vfjbfr8laQyRaNuwufHItFQExBeJrn', 
-  access_token_secret: 'So08FRJN6BbVnJR5ztAuHONndXgixN6w0BGYC4FlZipQ3'
-};
