000544cae414c8e093edbab2e7d326ea39bb8284
Roman Suarez <romandelsolar@gmail.com>
2014-11-07 03:59:31 -0300

    graphs

--- client/app/scripts/main.controller.js
+++ client/app/scripts/main.controller.js
@@ -1,8 +1,8 @@
 angular.module('toolnetApp')
 .controller('MainCtrl', function ($scope, $rootScope, $resource, $cookies, $routeParams, $http, $window, localStorageService) {
   
-  //var backUrl = '//localhost:3000';
-  var backUrl = '//romansuarez.com.ar';
+  var backUrl = '//localhost:3000';
+  //var backUrl = '//romansuarez.com.ar';
 
 	var redirectToLogin = function(){
     console.log("redirectToLogin");
@@ -75,6 +75,96 @@ angular.module('toolnetApp')
       patient.daysUsed = (new Date().getTime() - $scope.user.creation) / 1000 / 60 / 60 / 24;
     }
     $scope.activePatient = patient;
+    setTimeout(function() {
+      $('#chart-container').highcharts({
+          title: {
+              text: ''
+          },
+          xAxis: {
+              labels: {
+                  formatter: function() {
+                      return this.value + ' veces';
+                  }
+              }
+          },
+          yAxis: {
+              title: {
+                  text: 'Repeticiones'
+              },
+              plotLines: [{
+                  value: 0,
+                  width: 1,
+                  color: '#808080'
+              }]
+          },
+          tooltip: {
+              valueSuffix: ' repeticiones'
+          },
+          legend: {
+              layout: 'vertical',
+              align: 'right',
+              verticalAlign: 'middle',
+              borderWidth: 0
+          },
+          series: [{
+              name: 'Cierres de mano',
+              data: patient.data.games.closedHands
+          }, {
+              name: 'Flexion de codo',
+              data: patient.data.games.elbowFlexions
+          }, {
+              name: 'Giro izquierda',
+              data: patient.data.games.rotationsLeft
+          }, {
+              name: 'Giro derecha',
+              data: patient.data.games.rotationsRight
+          }]
+      }); 
+      $('#chart-container2').highcharts({
+          title: {
+              text: ''
+          },
+          xAxis: {
+              labels: {
+                  formatter: function() {
+                      return this.value + ' veces';
+                  }
+              }
+          },
+          yAxis: {
+              title: {
+                  text: 'Repeticiones'
+              },
+              plotLines: [{
+                  value: 0,
+                  width: 1,
+                  color: '#808080'
+              }]
+          },
+          tooltip: {
+              valueSuffix: ' repeticiones'
+          },
+          legend: {
+              layout: 'vertical',
+              align: 'right',
+              verticalAlign: 'middle',
+              borderWidth: 0
+          },
+          series: [{
+              name: 'Cierres de mano',
+              data: patient.data.tests.closedHands
+          }, {
+              name: 'Flexion de codo',
+              data: patient.data.tests.elbowFlexions
+          }, {
+              name: 'Giro izquierda',
+              data: patient.data.tests.rotationsLeft
+          }, {
+              name: 'Giro derecha',
+              data: patient.data.tests.rotationsRight
+          }]
+      });
+    }, 200);
   };
 
   $scope.closeShowPatient = function(){
@@ -89,11 +179,50 @@ angular.module('toolnetApp')
     return counter;
   }
 
+  function parsePatients(patients) {
+    patients.forEach(function(patient){
+      patient.data = {
+        games: {
+          closedHands: [0],
+          elbowFlexions: [0],
+          percentageInTrack: [0],
+          rotationsLeft: [0],
+          rotationsRight: [0]
+        },
+        tests: {
+          closedHands: [0],
+          elbowFlexions: [0],
+          percentageInTrack: [0],
+          rotationsLeft: [0],
+          rotationsRight: [0]
+        }
+      };
+      patient.games.forEach(function(game){
+        patient.data.games.closedHands.push(game.closedHands);
+        patient.data.games.elbowFlexions.push(game.elbowFlexions);
+        patient.data.games.percentageInTrack.push(game.percentageInTrack);
+        patient.data.games.rotationsLeft.push(game.rotationsLeft);
+        patient.data.games.rotationsRight.push(game.rotationsRight);
+      });
+      patient.performanceTest.forEach(function(game){
+        patient.data.tests.closedHands.push(game.closedHands);
+        patient.data.tests.elbowFlexions.push(game.elbowFlexions);
+        patient.data.tests.percentageInTrack.push(game.percentageInTrack);
+        patient.data.tests.rotationsLeft.push(game.rotationsLeft);
+        patient.data.tests.rotationsRight.push(game.rotationsRight);
+      });
+    });
+    return patients;
+  }
+
   function getPatients () {
     var uri = backUrl + '/users/patients';
     $http.get(uri).success(function(patients) {
+      console.log("java patients");
+      console.log(patients);
       $scope.allGames = countArrayProperty(patients, 'games');
-      $scope.patients = patients;
+      $scope.patients = parsePatients(patients);
+      console.log(patients);
     }).error(function() {
         return redirectToLogin();
     });
